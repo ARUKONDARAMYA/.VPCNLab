ea78e2ab73218f484ca65b68261133006273a53a
SaiHemanthBR <ssgahemanth15@hotmail.com>
2019-12-19 15:23:17 +0530

    Updated 19-12-2019 Lab Programs

---
+++ 19-12-2019/WonderWorksMagicShow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WonderWorksMagicShow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float total_people = 0;
+            float total_rating = 0.0f;
+
+            for(int i = 1; i < 4; i++)
+            {
+                Console.WriteLine($"Enter the number of people who watched show {i}");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Enter rating fo show {i}");
+                float r = float.Parse(Console.ReadLine());
+
+                total_people += n;
+                total_rating += n * r;
+            }
+
+            Console.WriteLine($"The overall average for show in {total_rating / total_people}");
+        }
+    }
+}
